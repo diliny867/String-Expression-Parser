b3eb0fe7f3ed9b79d64560bc7f6d20022fe96e7e
Ilja <100518672+diliny867@users.noreply.github.com>
2023-04-02 21:29:55 +0300

    no, parsing xlog like x and log is stupid

--- Expression String Parser/Source.cpp
+++ Expression String Parser/Source.cpp
@@ -7,6 +7,7 @@
 int main() {
 	ExprStrParser::Parser parser;
 	std::string expr = "-0.17*(25.215x*(-2+20)/(9-1))-log(25)^6+4!";
+	//std::string expr = "log(x)";
 
 	//std::map<std::string, float> args{{"x", 2.0f} };
 
